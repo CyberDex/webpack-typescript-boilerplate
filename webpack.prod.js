86abb7cc4e9480b33de35abf10931ebbe53da970
Dmytro Soldatov <cyberdexx@gmail.com>
2020-04-10 11:21:56 +0300

    init commit

---
+++ webpack.prod.js
@@ -0,0 +1,53 @@
+const TerserPlugin = require('terser-webpack-plugin')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const HTMLWebpackPlugin = require('html-webpack-plugin')
+const merge = require('webpack-merge');
+const common = require('./webpack.common.js')
+const build = require('../build.json')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const JavaScriptObfuscator = require('webpack-obfuscator')
+
+module.exports = merge(common, {
+    mode: 'production',
+    output: {
+        filename: 'js/[name].[contenthash].bundle.js',
+        chunkFilename: 'js/[name].[contenthash].chunk.js'
+    },
+    optimization: {
+        splitChunks: {
+            cacheGroups: {
+                commons: {
+                    filename: '[name].[contenthash].bundle.js'
+                }
+            }
+        }
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new JavaScriptObfuscator(
+            {
+                rotateStringArray: true,
+                stringArray: true,
+                stringArrayThreshold: 0.75
+            },
+            ['vendors.*.js']
+        ),
+        new TerserPlugin({
+            parallel: true,
+            terserOptions: {
+                ecma: 6,
+            },
+        }),
+        new CopyWebpackPlugin([{
+            "from": build.assetsFolder,
+            "ignore": [build.indexHTML]
+        }]),
+        new HTMLWebpackPlugin({
+            template: build.indexHTML,
+            templateParameters: build,
+            minify: {
+                collapseWhitespace: true
+            }
+        })
+    ]
+})
