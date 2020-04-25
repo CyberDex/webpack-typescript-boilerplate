8758148b3477ffbb2a420161c71abeb86d9a9a65
Dmytro Soldatov <cyberdexx@gmail.com>
2020-04-25 12:25:41 +0300

    feat: add tsconfig-paths-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -1,11 +1,13 @@
 const { CheckerPlugin } = require('awesome-typescript-loader')
 const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin')
 
 module.exports = config => ({
     mode: process.env.NODE_ENV,
     entry: config.entryPoints,
     resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.jsx']
+        extensions: ['.ts', '.tsx', '.js', '.jsx'],
+        plugins: [new TsconfigPathsPlugin()]
     },
     module: {
         rules: [{
